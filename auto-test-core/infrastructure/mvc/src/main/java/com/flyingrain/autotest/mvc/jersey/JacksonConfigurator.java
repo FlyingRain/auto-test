2a4f633e81cb698e17dd86c45562a4dffb157cd6
wally <1198420279@qq.com>
2024-04-23 11:14:40 +0800

    fxi bug

--- auto-test-core/infrastructure/mvc/src/main/java/com/flyingrain/autotest/mvc/jersey/JacksonConfigurator.java
+++ auto-test-core/infrastructure/mvc/src/main/java/com/flyingrain/autotest/mvc/jersey/JacksonConfigurator.java
@@ -1,5 +1,6 @@
 package com.flyingrain.autotest.mvc.jersey;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
@@ -8,6 +9,7 @@ import java.text.SimpleDateFormat;
 public class JacksonConfigurator extends JacksonJaxbJsonProvider {
     public JacksonConfigurator() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         setMapper(mapper);
     }
